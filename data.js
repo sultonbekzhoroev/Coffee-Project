dbd4ee62d57fd8d66e59c5fda549e169233e213c
sultonbek <zhoroevsultonbek@gmail.com>
2021-12-23 11:41:27 +0300

    commit

--- data.js
+++ data.js
@@ -1,144 +1,183 @@
-export const getData = function () {
-  return [
-    {
-      title: "Black",
-      description:
-        "Black coffee is as simple as it gets with ground coffee beans steeped in hot water, served warm. And if you want to sound fancy, you can call black coffee by its proper name: cafe noir.",
-      ingredients: ["Coffee"],
-      id: 1,
-    },
-    {
-      title: "Latte",
-      description:
-        "As the most popular coffee drink out there, the latte is comprised of a shot of espresso and steamed milk with just a touch of foam. It can be ordered plain or with a flavor shot of anything from vanilla to pumpkin spice.",
-      ingredients: ["Espresso", "Steamed Milk"],
-      id: 2,
-    },
-    {
-      title: "Cappuccino",
-      description:
-        "Cappuccino is a latte made with more foam than steamed milk, often with a sprinkle of cocoa powder or cinnamon on top. Sometimes you can find variations that use cream instead of milk or ones that throw in flavor shot, as well.",
-      ingredients: ["Espresso", "Steamed Milk", "Foam"],
-      id: 3,
-    },
-    {
-      title: "Americano",
-      description:
-        "With a similar flavor to black coffee, the americano consists of an espresso shot diluted in hot water.",
-      ingredients: ["Espresso", "Hot Water"],
-      id: 4,
-    },
-    {
-      title: "Espresso",
-      description:
-        "An espresso shot can be served solo or used as the foundation of most coffee drinks, like lattes and macchiatos.",
-      ingredients: ["1oz Espresso"],
-      id: 5,
-    },
-    {
-      title: "Doppio",
-      description:
-        "A double shot of espresso, the doppio is perfect for putting extra pep in your step.",
-      ingredients: ["2oz Espresso"],
-      id: 6,
-    },
-    {
-      title: "Cortado",
-      description:
-        "Like yin and yang, a cortado is the perfect balance of espresso and warm steamed milk. The milk is used to cut back on the espresso’s acidity.",
-      ingredients: ["1oz Espresso", "1oz Steamed Milk"],
-      id: 7,
-    },
-    {
-      title: "Red Eye",
-      description:
-        "Named after those pesky midnight flights, a red eye can cure any tiresome morning. A full cup of hot coffee with an espresso shot mixed in, this will definitely get your heart racing.",
-      ingredients: ["Coffee", "Espresso"],
-      id: 8,
-    },
-    {
-      title: "Galão",
-      description:
-        "Originating in Portugal, this hot coffee drink is closely related to the latte and cappuccino. Only difference is it contains about twice as much foamed milk, making it a lighter drink compared to the other two.",
-      ingredients: ["Espresso", "Foamed milk"],
-      id: 9,
-    },
-    {
-      title: "Lungo",
-      description:
-        "A lungo is a long-pull espresso. The longer the pull, the more caffeine there is and the more ounces you can enjoy.",
-      ingredients: ["Long pulled espresso"],
-      id: 10,
-    },
-    {
-      title: "Macchiato",
-      description:
-        "The macchiato is another espresso-based drink that has a small amount of foam on top. It’s the happy medium between a cappuccino and a doppio.",
-      ingredients: ["Espresso", "Foam"],
-      id: 11,
-    },
-    {
-      title: "Mocha",
-      description:
-        "For all you chocolate lovers out there, you’ll fall in love with a mocha (or maybe you already have). The mocha is a chocolate espresso drink with steamed milk and foam.",
-      ingredients: ["Espresso", "Steamed Milk", "Chocolate"],
-      id: 12,
-    },
-    {
-      title: "Ristretto",
-      description:
-        "Ristretto is an espresso shot. It uses less hot water which creates a sweeter flavor compared to the bitter taste of a traditional shot of espresso or a doppio.",
-      ingredients: ["Short pulled espresso"],
-      id: 13,
-    },
-    {
-      title: "Flat White",
-      description:
-        "This Aussie-born drink is basically a cappuccino without the foam or chocolate sprinkle. It’s an espresso drink with steamed milk.",
-      ingredients: ["Espresso", "Steamed Milk"],
-      id: 14,
-    },
-    {
-      title: "Affogato",
-      description:
-        "The affogato is an excuse to enjoy a scoop of ice cream any time of day (and any time of year in my opinion). Served with a scoop of ice cream and a shot of espresso, or two.",
-      ingredients: ["Espresso", "Ice cream"],
-      id: 15,
-    },
-    {
-      title: "Café au Lait",
-      description:
-        "Café au lait is perfect for the coffee minimalist who wants a bit more flavor. Just add a splash of warm milk to your coffee and you’re all set!",
-      ingredients: ["Coffee", "Steamed Milk"],
-      id: 16,
-    },
-    {
-      title: "Irish",
-      description:
-        "Irish coffee consists of black coffee, whiskey and sugar, topped with whipped cream.",
-      ingredients: ["Coffee", "Whiskey", "Sugar", "Cream"],
-      id: 17,
-    },
-    {
-      title: "Guayoyo",
-      description:
-        "Traditional venezuelan coffee prepared by filtering the ground coffee in a cone of cloth and pouring hot water on top of it. It's prefferably drinked wihout milk nor cream.",
-      ingredients: ["Coffee", "Traditional", "Hot Water"],
-      id: 18,
-    },
-    {
-      title: "Cortadito",
-      description:
-        "Traditional cuban coffee method where a bit of freshly brewed coffee is mixed with sugar to create a highly sugared paste. Then add the rest of the coffee and stir adding milk until a 50/50 ratio is achieved.",
-      ingredients: ["Coffee", "Traditional", "Sugar", "Milk"],
-      id: 19,
-    },
-    {
-      title: "Aguapanela Coffee",
-      description:
-        "Bring panela and coffee to a boil in a small pan for 30 minutes until panela is melted. Brew your coffee using your favorite brewing technique but add the hot aguapanela instead of hot water. Delicious sweetened coffee is ready.",
-      ingredients: ["Coffee", "Sweet", "Panela", "Traditional"],
-      id: 20,
-    },
-  ];
-};
+const data = [
+  {
+    title: "Black",
+    description:
+      "Black coffee is as simple as it gets with ground coffee beans steeped in hot water, served warm. And if you want to sound fancy, you can call black coffee by its proper name: cafe noir.",
+    ingredients: ["Coffee"],
+    id: 1,
+    image:
+      "https://www.healthifyme.com/blog/wp-content/uploads/2019/09/Black-coffee-feature-image.jpg",
+  },
+  {
+    title: "Latte",
+    description:
+      "As the most popular coffee drink out there, the latte is comprised of a shot of espresso and steamed milk with just a touch of foam. It can be ordered plain or with a flavor shot of anything from vanilla to pumpkin spice.",
+    ingredients: ["Espresso", "Steamed Milk"],
+    id: 2,
+    image:
+      "https://www.caffesociety.co.uk/assets/recipe-images/latte-small.jpg",
+  },
+  {
+    title: "Cappuccino",
+    description:
+      "Cappuccino is a latte made with more foam than steamed milk, often with a sprinkle of cocoa powder or cinnamon on top. Sometimes you can find variations that use cream instead of milk or ones that throw in flavor shot, as well.",
+    ingredients: ["Espresso", "Steamed Milk", "Foam"],
+    id: 3,
+    image:
+      "https://www.thespruceeats.com/thmb/UA_NNtGiS5n6CI07kJVOMkSqYEA=/2942x2942/smart/filters:no_upscale()/how-to-make-cappuccinos-766116-hero-01-a754d567739b4ee0b209305138ecb996.jpg",
+  },
+  {
+    title: "Americano",
+    description:
+      "With a similar flavor to black coffee, the americano consists of an espresso shot diluted in hot water.",
+    ingredients: ["Espresso", "Hot Water"],
+    id: 4,
+    image:
+      "https://enjoyjava.com/wp-content/uploads/2021/08/how-to-make-an-americano.jpg",
+  },
+  {
+    title: "Espresso",
+    description:
+      "An espresso shot can be served solo or used as the foundation of most coffee drinks, like lattes and macchiatos.",
+    ingredients: ["1oz Espresso"],
+    id: 5,
+    image:
+      "https://www.seriouseats.com/thmb/8MBQQ3KUKhlGwwRUlPuuoR-9c7Q=/1500x1125/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__serious_eats__seriouseats.com__2018__06__20180613-coffee-vs-espresso-vicky-wasik-3-1500x1125-418fa2a14e7249b18040c2c34bf8569c.jpg",
+  },
+  {
+    title: "Doppio",
+    description:
+      "A double shot of espresso, the doppio is perfect for putting extra pep in your step.",
+    ingredients: ["2oz Espresso"],
+    id: 6,
+    image:
+      "https://64.media.tumblr.com/d513fa3b4faa873684a9941d9a411864/tumblr_njpdmqFRlm1ungxeyo1_500.jpg",
+  },
+  {
+    title: "Cortado",
+    description:
+      "Like yin and yang, a cortado is the perfect balance of espresso and warm steamed milk. The milk is used to cut back on the espresso’s acidity.",
+    ingredients: ["1oz Espresso", "1oz Steamed Milk"],
+    id: 7,
+    image:
+      "https://i.pinimg.com/736x/37/ed/43/37ed43062b93b17a728b0c28ad0aea7d.jpg",
+  },
+  {
+    title: "Red Eye",
+    description:
+      "Named after those pesky midnight flights, a red eye can cure any tiresome morning. A full cup of hot coffee with an espresso shot mixed in, this will definitely get your heart racing.",
+    ingredients: ["Coffee", "Espresso"],
+    id: 8,
+    image:
+      "https://thecoffeeguru.net/wp-content/uploads/2021/05/RedEyeCoffeeDrink.jpg",
+  },
+  {
+    title: "Galão",
+    description:
+      "Originating in Portugal, this hot coffee drink is closely related to the latte and cappuccino. Only difference is it contains about twice as much foamed milk, making it a lighter drink compared to the other two.",
+    ingredients: ["Espresso", "Foamed milk"],
+    id: 9,
+    image: "https://www.portugalist.com/wp-content/uploads/galao-coffee.jpg",
+  },
+  {
+    title: "Lungo",
+    description:
+      "A lungo is a long-pull espresso. The longer the pull, the more caffeine there is and the more ounces you can enjoy.",
+    ingredients: ["Long pulled espresso"],
+    id: 10,
+    image:
+      "https://www.tangycoffee.com/wp-content/uploads/2020/10/coffee-2153572_1280.jpg",
+  },
+  {
+    title: "Macchiato",
+    description:
+      "The macchiato is another espresso-based drink that has a small amount of foam on top. It’s the happy medium between a cappuccino and a doppio.",
+    ingredients: ["Espresso", "Foam"],
+    id: 11,
+    image:
+      "https://www.thespruceeats.com/thmb/GiwOFlIXe6g8J1Z4Eem1rKtAcio=/3089x1738/smart/filters:no_upscale()/85153452-56a176765f9b58b7d0bf84dd.jpg",
+  },
+  {
+    title: "Mocha",
+    description:
+      "For all you chocolate lovers out there, you’ll fall in love with a mocha (or maybe you already have). The mocha is a chocolate espresso drink with steamed milk and foam.",
+    ingredients: ["Espresso", "Steamed Milk", "Chocolate"],
+    id: 12,
+    image:
+      "https://www.thespruceeats.com/thmb/Fh9f4uXLQUFGtGcgymrdyl2Uq3w=/1793x1345/smart/filters:no_upscale()/mocha-4797918-05-f664d7225d5a4b14abdeacd2bf48471f.jpg",
+  },
+  {
+    title: "Ristretto",
+    description:
+      "Ristretto is an espresso shot. It uses less hot water which creates a sweeter flavor compared to the bitter taste of a traditional shot of espresso or a doppio.",
+    ingredients: ["Short pulled espresso"],
+    id: 13,
+    image: "https://ineedcoffee.com/wp-content/uploads/2007/09/IMG_9259.jpg",
+  },
+  {
+    title: "Flat White",
+    description:
+      "This Aussie-born drink is basically a cappuccino without the foam or chocolate sprinkle. It’s an espresso drink with steamed milk.",
+    ingredients: ["Espresso", "Steamed Milk"],
+    id: 14,
+    image:
+      "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/flat-white-d8ada0f.jpg?quality=90&resize=440,400",
+  },
+  {
+    title: "Affogato",
+    description:
+      "The affogato is an excuse to enjoy a scoop of ice cream any time of day (and any time of year in my opinion). Served with a scoop of ice cream and a shot of espresso, or two.",
+    ingredients: ["Espresso", "Ice cream"],
+    id: 15,
+    image:
+      "https://pinchandswirl.com/wp-content/uploads/2020/05/Affogato-sq.jpg",
+  },
+  {
+    title: "Café au Lait",
+    description:
+      "Café au lait is perfect for the coffee minimalist who wants a bit more flavor. Just add a splash of warm milk to your coffee and you’re all set!",
+    ingredients: ["Coffee", "Steamed Milk"],
+    id: 16,
+    image:
+      "https://218zzz2u9z8k37r9ob41kso0-wpengine.netdna-ssl.com/wp-content/uploads/2020/12/shosuke-takahashi-f6-XQoheD50-unsplash-1024x682.jpg",
+  },
+  {
+    title: "Irish",
+    description:
+      "Irish coffee consists of black coffee, whiskey and sugar, topped with whipped cream.",
+    ingredients: ["Coffee", "Whiskey", "Sugar", "Cream"],
+    id: 17,
+    image:
+      "https://bakeitwithlove.com/wp-content/uploads/2020/03/Irish-Coffee-lg-sq.jpg",
+  },
+  {
+    title: "Guayoyo",
+    description:
+      "Traditional venezuelan coffee prepared by filtering the ground coffee in a cone of cloth and pouring hot water on top of it. It's prefferably drinked wihout milk nor cream.",
+    ingredients: ["Coffee", "Traditional", "Hot Water"],
+    id: 18,
+    image: "https://pbs.twimg.com/media/DHwhS46XUAAfi-c.jpg",
+  },
+  {
+    title: "Cortadito",
+    description:
+      "Traditional cuban coffee method where a bit of freshly brewed coffee is mixed with sugar to create a highly sugared paste. Then add the rest of the coffee and stir adding milk until a 50/50 ratio is achieved.",
+    ingredients: ["Coffee", "Traditional", "Sugar", "Milk"],
+    id: 19,
+    image:
+      "https://www.luckybelly.com/wp-content/uploads/2020/09/7-Easy-Steps-to-Make-a-Cortadito.jpg",
+  },
+  {
+    title: "Aguapanela Coffee",
+    description:
+      "Bring panela and coffee to a boil in a small pan for 30 minutes until panela is melted. Brew your coffee using your favorite brewing technique but add the hot aguapanela instead of hot water. Delicious sweetened coffee is ready.",
+    ingredients: ["Coffee", "Sweet", "Panela", "Traditional"],
+    id: 20,
+    image:
+      "https://www.mycolombianrecipes.com/wp-content/uploads/2010/06/aguapanela.jpg",
+  },
+];
+
+for (i = 0; i < data.length; i++) {
+  console.log(data[i].image);
+}
--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 const searchBar = document.querySelector("#searchBar");
 const ul = document.querySelector("#products");
+const input = document.querySelector("input");
+const log = document.getElementById("values");
+
+input.addEventListener("keydown", keydownListener);
+
+// function keydownListener(e) {
+//   const arr = getData();
+// }
+
+for (i = 0; i < data.length; i++) {
+  console.log(data[i].image);
+}
