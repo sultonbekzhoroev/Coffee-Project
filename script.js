9748c0b7fee9a3ea8112a82bf661701431313c33
Sultonbek Zhoroev <85235911+sultonbekzhoroev@users.noreply.github.com>
2021-12-28 09:37:34 +0300

    changed

--- script.js
+++ script.js
@@ -19,22 +19,7 @@ function dataOut(data) {
     .join("");
 
   section.innerHTML = newDrinks;
-  // for (let i = 0; i < data.length; i++) {
-  //   const divOfElements = document.createElement("div");
-  //   const anchor = document.createElement("a");
-  //   const title = document.createElement("p");
-  //   const image = document.createElement("img");
-  //   divOfElements.classList.add("divs-background");
-  //   title.innerText = data[i].title;
-  //   image.src = data[i].image;
-  //   anchor.appendChild(title);
-  //   anchor.appendChild(image);
-  //   divOfElements.appendChild(anchor);
-  //   container.appendChild(divOfElements);
-  // }
-}
-
-// ya vot ne znayu perebor pravilnno rabotayet ili net vi proverte
+  }
 const filterCoffee = (searchedName) => {
   const filteredData = [];
   for (let i = 0; i < data.length; i++) {
@@ -51,21 +36,11 @@ const filterCoffee = (searchedName) => {
   }
   return filteredData;
 
-  // const filteredData = data.filter((coffeeObject) => {
-  //   const drinkName = coffeeObject.title.toLowerCase();
-  //   return drinkName.includes(searchedName.toLowerCase());
-  // });
-  // return filteredData;
 };
 
-// vsegda pisat nuzhno fnction pered tem kak vizivat
-// inorder to load all data on first page
 dataOut(filterCoffee(""));
 
 searchBar.addEventListener("keyup", (e) => {
   const searchString = e.target.value.toLowerCase();
-  // on kajdiy raz izmenayet ineer html izza etogo udallyat ne nuzhno
   console.log(dataOut(filterCoffee(searchString)));
-
-  // console.log(searchString);
 });
