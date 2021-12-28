781929e4deb96f8a5b7038499df65f0e634f694e
sultonbek <zhoroevsultonbek@gmail.com>
2021-12-28 09:22:19 +0300

    last cmmit

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ import { dataGet } from "./data.js";
 const data = dataGet();
 const container = document.querySelector(".container");
 const searchBar = document.querySelector(".search-form");
-const button = document.querySelector(".btn");
+
 function dataOut(data) {
   const section = document.querySelector(".section-center");
 
@@ -19,28 +19,13 @@ function dataOut(data) {
     .join("");
 
   section.innerHTML = newDrinks;
-  // for (let i = 0; i < data.length; i++) {
-  //   const divOfElements = document.createElement("div");
-  //   const anchor = document.createElement("a");
-  //   const title = document.createElement("p");
-  //   const image = document.createElement("img");
-  //   divOfElements.classList.add("divs-background");
-  //   title.innerText = data[i].title;
-  //   image.src = data[i].image;
-  //   anchor.appendChild(title);
-  //   anchor.appendChild(image);
-  //   divOfElements.appendChild(anchor);
-  //   container.appendChild(divOfElements);
-  // }
 }
 
-// ya vot ne znayu perebor pravilnno rabotayet ili net vi proverte
 const filterCoffee = (searchedName) => {
   const filteredData = [];
   for (let i = 0; i < data.length; i++) {
     const drinkName = data[i].title.toLowerCase();
 
-    // if searhcString is ""
     if (searchedName == "") {
       return data;
     }
@@ -50,22 +35,12 @@ const filterCoffee = (searchedName) => {
     }
   }
   return filteredData;
-
-  // const filteredData = data.filter((coffeeObject) => {
-  //   const drinkName = coffeeObject.title.toLowerCase();
-  //   return drinkName.includes(searchedName.toLowerCase());
-  // });
-  // return filteredData;
 };
 
-// vsegda pisat nuzhno fnction pered tem kak vizivat
-// inorder to load all data on first page
 dataOut(filterCoffee(""));
 
 searchBar.addEventListener("keyup", (e) => {
   const searchString = e.target.value.toLowerCase();
-  // on kajdiy raz izmenayet ineer html izza etogo udallyat ne nuzhno
-  console.log(dataOut(filterCoffee(searchString)));
 
-  // console.log(searchString);
+  console.log(dataOut(filterCoffee(searchString)));
 });
