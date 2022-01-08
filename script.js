b9f058c39e905f3408ca1b4146e71d009c2d6ec0
sultonbek <zhoroevsultonbek@gmail.com>
2022-01-08 18:32:21 +0300

    commit

--- script.js
+++ script.js
@@ -12,10 +12,10 @@ function dataOut(data) {
     .map((drink) => {
       const { title: name, image: image } = drink;
       return `<a href="./link.html" >
-                  <img src="${image}" alt="${name}" />
-                  <h3>${name}</h3>
-               </article>
-       </a>`;
+   <img src="${image}" alt="${name}" />
+   <h3>${name}</h3>
+   </article>
+   </a>`;
     })
     .join("");
 
@@ -40,55 +40,4 @@ dataOut(filterCoffee(""));
 
 searchBar.addEventListener("keyup", (e) => {
   const searchString = e.target.value.toLowerCase();
-  console.log(dataOut(filterCoffee(searchString)));
-});
-
-/*anara*/
-/* responsive */
-
-const navToggle = document.querySelector(".nav-toggle");
-const headerMenu = document.querySelector("#header_menu");
-const form = document.querySelector(".form");
-const section = document.querySelector(".section-center");
-const article = document.querySelector("article");
-const header = document.querySelector("header");
-const h2 = document.querySelector("h2");
-const closeMenu = document.querySelector(".closeMenu");
-const ul = document.querySelector("ul");
-
-navToggle.addEventListener("click", () => {
-  if (ul.classList.contains("show-header-menu")) {
-    ul.classList.remove("show-header-menu");
-    section.style.display = "block";
-    section.style.height = "auto";
-    ul.style.display = "none";
-    form.style.paddingTop = "50px";
-    h2.style.display = "block";
-  } else {
-    ul.classList.add("show-header-menu");
-    ul.style.paddingTop = "300px";
-    h2.style.display = "none";
-    ul.style.display = "block";
-    ul.style.height = "100vh";
-
-    //section.style.display = "none";
-  }
-});
-/* 
-    navToggle.addEventListener("click", () => {
-        headerMenu.style.display = "block";
-        headerMenu.style.top = "0";
-        headerMenu.style.height = "100vh"
-        section.style.display = "none"
-
-
-    });
-    closeMenu.addEventListener("click", () => {
-        headerMenu.style.top = "-100%";
-
-    }); */
-
-window.addEventListener("load", function () {
-  const loadWrapper = this.document.querySelector(".load-wrapper");
-  loadWrapper.parentElement.removeChild(loadWrapper);
 });
