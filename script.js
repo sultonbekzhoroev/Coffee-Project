ab907383b6d7866d18fa2e6c9855b23f7ff4a6fc
sultonbek <zhoroevsultonbek@gmail.com>
2022-01-14 10:08:56 +0300

    final commit

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 import { dataGet } from "./data.js";
-console.log("script.js");
 
 const data = dataGet();
 const searchBar = document.querySelector(".search-form");
@@ -11,15 +10,15 @@ function dataOut(data) {
 
   const newDrinks = data
     .map((drink) => {
-      const { title: name, image: image, id: id } = drink;
-      return `<a class = "items">
-   <img src="${image}" alt="${name}" id = "${id}"/>
-    <h3>${name}</h3>
-    </article>
-    </a>`;
+      const { title: name, image: image } = drink;
+      return `<a href="./link.html" >
+   <img src="${image}" alt="${name}" />
+   <h3>${name}</h3>
+   </article>
+   </a>`;
     })
     .join("");
-  // href="./link.html"
+
   section.innerHTML = newDrinks;
 }
 const filterCoffee = (searchedName) => {
@@ -49,18 +48,6 @@ window.addEventListener("load", function () {
   loadWrapper.parentElement.removeChild(loadWrapper);
 });
 
-// Получить модальный
-let modal = document.getElementById("myModal");
-
-// Получить кнопку, которая открывает модальный
-let btn = document.getElementById("myBtn");
-
-// Получить элемент <span>, который закрывает модальный
-let span = document.getElementsByClassName("close")[0];
-
-// //store submit button
-let register = document.querySelector(".registerbtn");
-
 /* filter btn */
 
 const btnContainer = document.querySelector(".btn-container");
@@ -83,8 +70,8 @@ function displayMenuButtons() {
   const categoryBtns = categories
     .map(function (category) {
       return `<button type="button" class="filter-btn" data-id=${category}>
-            ${category}
-          </button>`;
+          ${category}
+        </button>`;
     })
     .join("");
 
@@ -111,12 +98,64 @@ function displayMenuButtons() {
   });
 }
 
+// Получить модальный
+const modal = document.getElementById("myModal");
+
+// получаем кнопку регистрации
+const registerBtn = document.querySelector(".registerbtn");
+
+// Ставим флаг на то, зареган пользватель или нет
+let isRegistered = false;
+
+// Приветственное сообщение после регистрации
+const welcomeMessage = document.querySelector(".welcome-message");
+/* Все инпуты взяты в отдельный блок с классом form-content чтобы этот блок легко было скрыть
+ */
+const formContent = document.querySelector(".form-content");
+
+// Получить кнопку, которая открывает модальный
+let butn = document.getElementById("myBtn");
+
+// Получить элемент <span>, который закрывает модальный
+let span = document.getElementsByClassName("close")[0];
+
+// //store submit button
+let register = document.querySelector(".registerbtn");
+
+// //store message after submit
+// let message = document.querySelector("#response");
+
+// Когда пользователь нажимает на кнопку, откройте модальный
+butn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// Когда пользователь нажимает на <span> (x), закройте модальное окно
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// Когда пользователь щелкает в любом месте за пределами модального, закройте его
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+registerBtn.addEventListener("click", function (event) {
+  event.preventDefault(); // отменяем стандартное поведение браузера
+  isRegistered = true; // по клику флаг зарегистрирован, переводим в true
+  welcomeMessage.style.display = "block"; // показываем привественное сообщение
+  formContent.style.display = "none"; // скрываем весь блок с инпутами
+});
+
 const navToggle = document.querySelector(".nav-toggle");
 const form = document.querySelector(".form");
 const section = document.querySelector(".section-center");
 const h2 = document.querySelector("h2");
 const ul = document.querySelector("ul");
 const logoImg = document.querySelector("img");
+// const giftBtn = document.querySelector("#myBtn");
 
 navToggle.addEventListener("click", () => {
   if (ul.classList.contains("show-header-menu")) {
@@ -126,6 +165,7 @@ navToggle.addEventListener("click", () => {
     ul.style.display = "none";
     form.style.paddingTop = "50px";
     h2.style.display = "block";
+    // giftBtn.display = "center";
   } else {
     ul.classList.add("show-header-menu");
     ul.style.paddingTop = "300px";
